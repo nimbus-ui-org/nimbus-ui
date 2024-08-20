af24050c56e3cb859e5eea7a50b5b12170585d89
Mohamad Jawhar <mohamad.jaouhar98@gmail.com>
2024-08-20 18:50:43 -0400

    add tsconfig prettier and eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,46 @@
+/**
+ * @type {import("@types/eslint").Linter.BaseConfig}
+ */
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:prettier/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module'
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  env: {
+    es6: true,
+    browser: true,
+    node: true
+  },
+  plugins: ['prettier'],
+  globals: {
+    process: true
+  },
+  rules: {
+    '@typescript-eslint/no-unsafe-function-type': 'off',
+    'prettier/prettier': [
+      1,
+      {
+        semi: false,
+        trailingComma: 'none',
+        singleQuote: true,
+        printWidth: 120,
+        endOfLine: 'auto'
+      }
+    ]
+  }
+}
