61070a88a115657e9c289c23bda50db653dbecfa
Mohamad Jawhar <mohamad.jaouhar98@gmail.com>
2024-09-23 17:52:46 -0400

    update entries for components

--- packages/core/tsup.config.ts
+++ packages/core/tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig, type Options } from 'tsup'
 
 const commonOptions: Options = {
-  entry: ['src/index.ts', 'src/components/*/index.ts', 'src/preset/index.ts'],
+  entry: ['src/index.ts', 'src/components/**/*/index.ts', 'src/preset/index.ts'],
   banner: { js: '"use client";' }, // add this on top of every UI component since they are customizable with props
   sourcemap: true, // generate .map files
   clean: true,
