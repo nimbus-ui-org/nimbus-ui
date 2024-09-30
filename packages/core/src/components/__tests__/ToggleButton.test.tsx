f18f732bd7c91d6e53ba603d79c0a3697484416a
Mohamad Jawhar <mohamad.jaouhar98@gmail.com>
2024-09-30 17:39:25 -0400

    add ToggleButton tests

---
+++ packages/core/src/components/__tests__/ToggleButton.test.tsx
@@ -0,0 +1,68 @@
+import { ToggleButton, ToggleIconButton } from '@components/Buttons'
+import { testAccessibility } from './utils'
+import { render } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+
+describe('ToggleButton', () => {
+  testAccessibility(<ToggleButton aria-label="Nimus Toggle Button" />)
+  testAccessibility(<ToggleIconButton aria-label="Nimus Toggle Icon Button" />)
+
+  it('should add data-value attribute when value prop is specified', () => {
+    const getRender = (value?: string) => (
+      <>
+        <ToggleButton value={value} data-testid="toggle-button" />
+        <ToggleIconButton value={value} data-testid="toggle-icon-button" />
+      </>
+    )
+
+    const { getByTestId, rerender } = render(getRender())
+
+    const button = getByTestId('toggle-button')
+    const iconButton = getByTestId('toggle-icon-button')
+
+    expect(button).not.toHaveAttribute('data-value')
+    expect(iconButton).not.toHaveAttribute('data-value')
+
+    rerender(getRender('test-value'))
+    expect(button).toHaveAttribute('data-value')
+    const dataButtonValue = button.getAttribute('data-value')
+    expect(dataButtonValue).toBe('test-value')
+    expect(iconButton).toHaveAttribute('data-value')
+    const dataIconButtonValue = button.getAttribute('data-value')
+    expect(dataIconButtonValue).toBe('test-value')
+  })
+
+  it('should fire onChange passing isSelected and value', async () => {
+    const onChangeButton = vi.fn()
+    const onChangeIconButton = vi.fn()
+
+    const { getByTestId } = render(
+      <>
+        <ToggleButton
+          value="test-value"
+          onChange={onChangeButton}
+          data-testid="toggle-button"
+        />
+        <ToggleIconButton
+          value="test-value"
+          onChange={onChangeIconButton}
+          data-testid="toggle-icon-button"
+        />
+      </>
+    )
+
+    const button = getByTestId('toggle-button')
+    const iconButton = getByTestId('toggle-icon-button')
+
+    expect(onChangeButton).not.toHaveBeenCalled()
+    expect(onChangeIconButton).not.toHaveBeenCalled()
+
+    await userEvent.click(button)
+    expect(onChangeButton).toHaveBeenCalledTimes(1)
+    expect(onChangeButton).toHaveBeenNthCalledWith(1, true, 'test-value')
+
+    await userEvent.click(iconButton)
+    expect(onChangeIconButton).toHaveBeenCalledTimes(1)
+    expect(onChangeIconButton).toHaveBeenNthCalledWith(1, true, 'test-value')
+  })
+})
---
+++ packages/core/src/components/__tests__/ToggleButtonProvider.test.tsx
@@ -0,0 +1,48 @@
+import { ToggleButton, ToggleButtonProvider, ToggleIconButton } from '@components/Buttons'
+import { render } from '@testing-library/react'
+
+describe('ToggleButtonProvider', () => {
+  it('should pass down props to Toggle or ToggleIconButton components', () => {
+    const { getByTestId } = render(
+      <ToggleButtonProvider isDisabled>
+        <ToggleButton data-testid="toggle-button" />
+        <ToggleIconButton data-testid="toggle-icon-button" />
+      </ToggleButtonProvider>
+    )
+
+    const button = getByTestId('toggle-button')
+    const iconButton = getByTestId('toggle-icon-button')
+
+    expect(button).toHaveAttribute('data-disabled')
+    expect(button).toHaveAttribute('data-appearance-disabled')
+    expect(iconButton).toHaveAttribute('data-disabled')
+    expect(iconButton).toHaveAttribute('data-appearance-disabled')
+  })
+
+  it('should control the selected state when selectedValue is provided', () => {
+    const getRender = (value: string | null) => (
+      <ToggleButtonProvider selectedValue={value}>
+        <ToggleButton value="button-value" data-testid="toggle-button" />
+        <ToggleIconButton value="icon-button-value" data-testid="toggle-icon-button" />
+      </ToggleButtonProvider>
+    )
+
+    const { getByTestId, rerender } = render(getRender('button-value'))
+
+    const button = getByTestId('toggle-button')
+    const iconButton = getByTestId('toggle-icon-button')
+
+    expect(button).toHaveAttribute('data-selected')
+    expect(iconButton).not.toHaveAttribute('data-selected')
+
+    rerender(getRender('icon-button-value'))
+
+    expect(button).not.toHaveAttribute('data-selected')
+    expect(iconButton).toHaveAttribute('data-selected')
+
+    rerender(getRender(null))
+
+    expect(button).not.toHaveAttribute('data-selected')
+    expect(iconButton).not.toHaveAttribute('data-selected')
+  })
+})
